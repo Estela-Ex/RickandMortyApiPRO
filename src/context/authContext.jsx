380817f65f89afd1b01197c51599ea7c4de8dd46
Estela Extremera <estelaextremeracampos@gmail.com>
2023-05-12 14:01:25 +0200

    cambios  detailsHome with login

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState, createContext } from "react";
 
-const authContext = createContext({
+const AuthContext = createContext({
   login: () => {},
   logout: () => {},
   user: null,
@@ -8,16 +8,40 @@ const authContext = createContext({
 });
 
 const RICK_AND_MORTY_LOGIN = "RICK_AND_MORTY_LOGIN";
-export default function authContextProvider({ children }) {
+export default function AuthContextProvider({ children }) {
     const [user, setUser] = useState(() =>
       JSON.parse(localStorage.getItem(RICK_AND_MORTY_LOGIN)) ?? null
     );
-
+    const [errorMessage, setErrorMessage] = useState(null)
     function login(user) {
         if (
             user.name === "estela" &&
             user.email === "estela@gmail.com" &&
             user.password === "12345"
-        )
+        ) {
+            localStorage.setItem(RICK_AND_MORTY_LOGIN, JSON.stringify(user))
+            setUser(user);
+            setErrorMessage(null)
+        }
+        setErrorMessage("Las credenciales no son correctas")
     }
+
+    function logout() {
+        localStorage.removeItem(RICK_AND_MORTY_LOGIN);
+        setUser(null)
+    }
+
+    const value = {
+        user,
+        login,
+        logout,
+        errorMessage
+    }
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
 }
+
+export function useAuthContext() {
+    return useContext(AuthContext)
+}
+
--- src/views/Home/DetailsHome/DetailsHome.jsx
+++ src/views/Home/DetailsHome/DetailsHome.jsx
@@ -1,10 +1,38 @@
 import img from '../../../assets/render0001.png'
-
+import { TextField, Box, Button, Typography, Checkbox, Grid } from "@mui/material";
 export default function DetailsHome() {
-    return (
-      <div className='home-container'>
-        <h1>Bienvenidos a la api de Rick and Morty</h1>
-        <img className="home-img"src={img} />;
-      </div>
-    );
+ return (
+   <>
+     <h1 className="centrado">Bienvenidos a la api de Rick and Morty</h1>
+     <Grid container spacing={2}>
+       <Grid item xs={6} md={7}>
+         <img className="home-img" src={img} />
+       </Grid>
+       <Grid item xs={6} md={5}>
+         <Typography variant="h5" sx={{ mr: 16, mt: 16, textAlign: "center" }}>
+           Inicia Sesión
+         </Typography>
+         <Box
+           sx={{
+             display: "flex",
+             flexDirection: "column",
+             p: 2,
+             mr: 16,
+             border: "1px solid grey",
+             borderRadius: 2,
+           }}
+         >
+           <TextField label="Email" size="small" sx={{ mb: 2 }} />
+           <TextField label="Password" size="small" sx={{ mb: 2 }} />
+           <span>
+             Recordar contraseña <Checkbox defaultChecked />
+           </span>
+           <Button variant="contained" sx={{ mb: 2 }}>
+             Login
+           </Button>
+         </Box>
+       </Grid>
+     </Grid>
+   </>
+ );
 }
